a348958536f0893dac3c3ff5fcd33e6d1cc0c3f9
lucie carpentier & jonathan baron <lucy.carpe@gmail.com>
2015-11-24 16:29:15 +0100

    Sixieme tache in progress.

--- src/org/ulco/Layer.java
+++ src/org/ulco/Layer.java
@@ -13,9 +13,12 @@ public class Layer {
         m_list= new Vector<GraphicsObject>();
         String str = json.replaceAll("\\s+","");
         int objectsIndex = str.indexOf("objects");
+        int groupIndex =  str.indexOf("groups");
         int endIndex = str.lastIndexOf("}");
 
-        parseObjects(str.substring(objectsIndex + 9, endIndex - 1));
+       // parseObjects(str.substring(objectsIndex + 9, endIndex - 1));
+        parseObjects(str.substring(objectsIndex + 9, groupIndex -2));
+        parseObjects(str.substring(groupIndex + 8, endIndex -1));
     }
 
     public void add(GraphicsObject o) {
@@ -102,15 +105,25 @@ public class Layer {
 
     public String toJson() {
         String str = "{ type: layer, objects : { ";
+        String groupestr ="";
 
         for (int i = 0; i < m_list.size(); ++i) {
-            GraphicsObject element = m_list.elementAt(i);
-
-            str += element.toJson();
-            if (i < m_list.size() - 1) {
-                str += ", ";
+          //  GraphicsObject element = m_list.elementAt(i);
+            if (m_list.elementAt(i) instanceof Group){
+                Group element = (Group) m_list.elementAt(i);
+            groupestr += element.toJson();
+        }else
+            {
+                GraphicsObject element = m_list.elementAt(i);
+                str += element.toJson();
+                if (i < m_list.size() - 1) {
+                    str += ", ";
+                }
             }
+
         }
+        str += "},  groups : {";
+        str += groupestr;
         return str + " } }";
     }
 
